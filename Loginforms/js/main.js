4d7ca3d69d1783317b7b4d5832a7fc7544f99b50
Clayton Garcia <claaytongarcia@gmail.com>
2021-09-26 21:00:09 -0300

    inicio da implementaçao do frontend do mitmirror

--- Loginforms/js/main.js
+++ Loginforms/js/main.js
@@ -1,3 +1,32 @@
+function fazpost(url, body) {
+    var request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload = function() {
+        console.log(this.responseText)
+    }
+    window.document.getElementById('username').value = ''
+    window.document.getElementById('password').value = ''
+    return request.responseText
+}
+
+
+function enviar() {
+    event.preventDefault()
+    var url = "http://127.0.0.1:5000/api/auth/"
+    var username = window.document.getElementById('username').value
+    var password = window.document.getElementById('password').value
+
+    body = {
+        "username": username,
+        "password": password
+    }
+
+    fazpost(url, body)
+}
+
 const inputs = document.querySelectorAll('.input');
 
 function focusFunc(){
